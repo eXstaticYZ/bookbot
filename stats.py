3b6bb59103653e9bfada81f025baa67930c0732d
eXstaticYZ <exstaticyz@gmail.com>
2025-02-23 23:42:26 -0800

    bootdev bookbot

---
+++ stats.py
@@ -0,0 +1,15 @@
+def get_num_words(book):
+    return len(book.split())
+
+def get_char_counts(book):
+    letters = {}
+    for char in book:
+        letter = char.lower()
+        if char == "\n":
+            pass
+        elif letter in letters:
+            letters[letter] += 1
+        else:
+            letters[letter] = 1
+    letters = {key: val for key, val in sorted(letters.items(), key = lambda ele: ele[1], reverse = True)}
+    return letters
