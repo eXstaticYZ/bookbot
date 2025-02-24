4d59b1f9a11526af5013d176ee7a0981be5a5da5
eXstaticYZ <exstaticyz@gmail.com>
2025-02-24 00:08:56 -0800

    reformatted main()

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 import sys
 from stats import get_char_counts, get_num_words
 
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+    book = get_book_text(file_path)
+    word_count = get_num_words(book)
+    chars = get_char_counts(book)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at %s..." % file_path)
+    print("----------- Word Count ----------")
+    print("Found %s total words" % word_count)
+    print("--------- Character Count -------")
+    for char in chars:
+        print(f"{char}: {chars[char]}")
+    print("============= END ===============")
+
 def get_book_text(file_path):
     file = open(file_path, "r")
     contents = file.read()
     file.close()
     return contents
 
-if len(sys.argv) < 2:
-    print("Usage: python3 main.py <path_to_book>")
-    sys.exit(1)
-
-file_path = sys.argv[1]
-book = get_book_text(file_path)
-word_count = get_num_words(book)
-chars = get_char_counts(book)
-
-print("============ BOOKBOT ============")
-print("Analyzing book found at %s..." % file_path)
-print("----------- Word Count ----------")
-print("Found %s total words" % word_count)
-print("--------- Character Count -------")
-for char in chars:
-    print(f"{char}: {chars[char]}")
-print("============= END ===============")
+main()
